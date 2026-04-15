9963b3f42c371fafb53bebb6678b9e9bc87e6def
tbotnz <tonynealon1989@gmail.com>
2026-04-15 10:26:34 +0000

    refactor: update dependency functions to use HTTPConnection instead of Request

--- ui/dependencies.py
+++ ui/dependencies.py
@@ -1,12 +1,12 @@
-from fastapi import Request
 from fastapi.templating import Jinja2Templates
+from starlette.requests import HTTPConnection
 
 from ui.services.temporal import TemporalService
 
 
-def get_temporal_service(request: Request) -> TemporalService:
-    return request.app.state.temporal_service
+def get_temporal_service(conn: HTTPConnection) -> TemporalService:
+    return conn.app.state.temporal_service
 
 
-def get_templates(request: Request) -> Jinja2Templates:
-    return request.app.state.templates
+def get_templates(conn: HTTPConnection) -> Jinja2Templates:
+    return conn.app.state.templates
