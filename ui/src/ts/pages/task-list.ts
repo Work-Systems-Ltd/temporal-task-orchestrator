b8a2c57488a66f846be7fdd977001980e32569dd
tbotnz <tonynealon1989@gmail.com>
2026-04-20 05:20:22 +0000

    refactor: restore constants file for workflow/task list column definitions

--- ui/src/ts/pages/task-list.ts
+++ ui/src/ts/pages/task-list.ts
@@ -1,5 +1,5 @@
 import { createSocket, type SocketHandle } from "../api/ws";
-import { ALL_COLUMNS, COL_STORAGE_KEY, DEFAULT_COLUMNS } from "../shared/constants";
+import { ALL_COLUMNS, COL_STORAGE_KEY, DEFAULT_COLUMNS } from "../constants";
 
 interface ViewParams {
   type: "view";
---
+++ ui/static/js/constants.js
@@ -0,0 +1,1 @@
+(()=>{var t=["id","type","started","stopped","duration","status","queue","run_id","events","parent"],s=["id","type","started","duration","status"],e="wf-visible-cols";})();
